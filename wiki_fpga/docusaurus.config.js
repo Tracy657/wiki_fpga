2924d8f8e6906eef9737f88d840b683767a380e4
Tracy657 <2268637392@qq.com>
2024-12-26 03:55:19 +0800

    /测试

--- wiki_fpga/docusaurus.config.js
+++ wiki_fpga/docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 (module.exports = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://Tracy657.github.io/wiki_fpga',
+  url: 'https://Tracy657.github.io/wiki_fpga/',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
